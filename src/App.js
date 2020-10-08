a691830920f0f93eed8fc2ec189690e1e4871243
Umer Waseem <60791855+umerwaseem4@users.noreply.github.com>
2020-10-08 11:10:50 +0500

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import React from "react";
 import "./App.css";
-
+import Navbar from "./components/Navbar/Navbar";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./components/Pages/Home";
 function App() {
-  return <div className="App">Hello World</div>;
+  return (
+    <div>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route exact path="/" component={Home} />
+        </Switch>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
