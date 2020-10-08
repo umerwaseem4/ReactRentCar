b1d6b98a396a939f9b29bba181ba756ee0b65f5b
Umer Waseem <60791855+umerwaseem4@users.noreply.github.com>
2020-10-08 12:51:02 +0500

    created btn component

---
+++ src/components/Button/Button.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Button.css";
+
+const Button = (props) => {
+  return (
+    <div>
+      <button class="main__btn">
+        <Link to={props.path}>{props.title}</Link>
+      </button>
+    </div>
+  );
+};
+
+export default Button;
