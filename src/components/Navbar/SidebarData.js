300d5fb448159c39989469e54f36521eb31a76d2
Umer Waseem <60791855+umerwaseem4@users.noreply.github.com>
2020-10-08 13:36:21 +0500

    updates nav content

--- src/components/Navbar/SidebarData.js
+++ src/components/Navbar/SidebarData.js
@@ -29,12 +29,6 @@ export const SidebarData = [
     cName: "nav-text",
   },
   {
-    title: "Messages",
-    path: "/messages",
-    icon: <FaIcons.FaEnvelopeOpenText />,
-    cName: "nav-text",
-  },
-  {
     title: "Support",
     path: "/support",
     icon: <IoIcons.IoMdHelpCircle />,
