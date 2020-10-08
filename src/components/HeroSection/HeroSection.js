f3bfa1510b38d078159a228aa013136ff01b284b
Umer Waseem <60791855+umerwaseem4@users.noreply.github.com>
2020-10-08 12:51:24 +0500

    added more to hero section

--- src/components/HeroSection/HeroSection.js
+++ src/components/HeroSection/HeroSection.js
@@ -1,19 +1,19 @@
 import React from "react";
 import "./HeroSection.css";
 import heroImage from "../images/undraw_Mobile_pay_re_sjb8.svg";
-import { Link } from "react-router-dom";
-const HeroSection = () => {
+import Button from "../Button/Button";
+import Text from "../GradientText/Text";
+
+const HeroSection = (props) => {
   return (
     <div>
       <div class="main">
         <div class="main__container container">
           <div class="main__content">
-            <h1>NEXT GENERATION</h1>
-            <h2>Rent A Car</h2>
-            <p>Luxury on the way.</p>
-            <button class="main__btn">
-              <Link to="/">Get Started</Link>
-            </button>
+            <h1>{props.title}</h1>
+            <h2>{props.subTitle}</h2>
+            <p>{props.para}.</p>
+            <Button path="/" title="Get Started" />
           </div>
           <div class="main__img--container">
             <img id="main__img" src={heroImage} />
