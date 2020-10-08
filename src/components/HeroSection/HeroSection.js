34ac86f18c79951b0d63847d76ce1d00e23623a7
Umer Waseem <60791855+umerwaseem4@users.noreply.github.com>
2020-10-08 18:14:54 +0500

    added rellax effect

--- src/components/HeroSection/HeroSection.js
+++ src/components/HeroSection/HeroSection.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./HeroSection.css";
 import heroImage from "../images/undraw_order_a_car_3tww.svg";
 import Button from "../Button/Button";
+import Parallex from "react-rellax";
 
 const HeroSection = (props) => {
   return (
@@ -9,10 +10,12 @@ const HeroSection = (props) => {
       <div className="main">
         <div className="main__container container">
           <div className="main__content">
-            <h1 id="hero_h1">{props.title}</h1>
-            <h2>{props.subTitle}</h2>
-            <p>{props.para}.</p>
-            <Button path="/" title="Get Started" />
+            <Parallex speed={3}>
+              <h1 id="hero_h1">{props.title}</h1>
+              <h2>{props.subTitle}</h2>
+              <p>{props.para}.</p>
+              <Button path="/" title="Get Started" />
+            </Parallex>
           </div>
           <div className="main__img--container">
             <img id="main__img" src={heroImage} alt="home_img" />
--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Services.css";
+import Parallex from "react-rellax";
 
 const Services = () => {
   return (
@@ -7,11 +8,13 @@ const Services = () => {
       <div className="services">
         <h1>See what the hype is about</h1>
         <div className="services__container">
-          <div className="services__card">
-            <h2>Experience Bliss</h2>
-            <p>Luxury service on you doorstep.</p>
-            <button>Get Started</button>
-          </div>
+          <Parallex speed={1}>
+            <div className="services__card">
+              <h2>Experience Bliss</h2>
+              <p>Luxury service on you doorstep.</p>
+              <button>Get Started</button>
+            </div>
+          </Parallex>
           <div className="services__card">
             <h2>Are you Ready?</h2>
             <p>Take the leap</p>
